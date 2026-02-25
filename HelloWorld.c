41ab5cf5744507d4738a613efc5c4846510cab14
Atul Verma <atul.verma8910@gmail.com>
2026-02-26 01:30:51 +0530

    removed extra files

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("a is");
+    scanf("%f", &a);
+    printf("b is");
+    scanf("%f", &b);
+    printf("a multiplied by b is %f", a*b);
+    return 0;
+}
---
+++ TestByVivan.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("put a");
+    scanf("%d" , &a);
+    printf("a is %d", a);
+    return 0;
+}
